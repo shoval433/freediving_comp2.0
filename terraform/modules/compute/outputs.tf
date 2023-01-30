748dc04275105ee93a915420ef7a12d89fa13cc4
shoval433 <119498041+shoval433@users.noreply.github.com>
2023-01-30 12:55:21 +0200

    Update outputs.tf

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -3,9 +3,9 @@ output "key" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.prod_shoval_iac[count.index].public_ip
+  value = [for i in range(var.ec2-count) : aws_instance.prod_shoval_iac[i].public_ip]
 }
 
 output "ec2_id" {
-  value = aws_instance.prod_shoval_iac[count.index].id
+  value = [for i in range(var.ec2-count) : aws_instance.prod_shoval_iac[i].id]
 }
