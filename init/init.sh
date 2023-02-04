ab4a02f024075fbeeb76bbe4bdd726d7dbe0389f
shoval <shoval123055@gmail.com>
2023-02-04 10:40:14 +0200

    debug is fun ?

--- init/init.sh
+++ init/init.sh
@@ -21,14 +21,25 @@ check=0
 aws --version || { check=1 ; }
 sleep 1
 done
-
+echo "login-aws"
+sleep 5
 aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.eu-west-3.amazonaws.com
+echo "open tar"
+sleep 5
 tar -xvzf start_to_ec2.tar.gz
 tar -xvzf templates.tar.gz
+echo "tar is open"
+sleep 5
+echo "try docker-compose"
+sleep 5
  while [ ${check} == 1 ]
  do
  check=0
 sudo docker compose --version || { check=1 ; }
  sleep 1
  done
+echo "docker-compose is up"
+sleep 5
+echo "start app"
+sleep 5
 export VERSION_COMP=$1 && docker compose -f docker-compose-prod.yaml up --build -d
